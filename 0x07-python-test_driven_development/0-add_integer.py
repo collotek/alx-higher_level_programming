60ba37f5bfce3b2e13f40e09376d872d38d91816
collotek <collinskipkorir94@gmail.com>
2023-04-04 18:04:16 +0300

    error resolve

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """A function that adds two numbers"""
 
+
 def add_integer(a, b=98):
     """Return the result of adding a and b
     a and b are first casted into integers if float before addition
     Raises:
         TypeError: If a or b are neither floats or integers
     """
-    if (isinstance(a, int) == False and isinstance(a, float) == False):
+    if (isinstance(a, int) is False and isinstance(a, float) is False):
         raise TypeError("a must be an integer")
-    if (isinstance(b, int) == False and isinstance(b, float) == False):
-            raise TypeError("b must be an integer")
-    return int(a) + int (b)
-print(add_integer('string', 2))
-       
-    
+    if (isinstance(b, int) is False and isinstance(b, float) is False):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
