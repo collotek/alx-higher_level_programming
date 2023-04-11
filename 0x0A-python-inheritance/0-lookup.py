e916876baf798e684960069072be4fa5f7666d14
collotek <collinskipkorir94@gmail.com>
2023-04-11 18:23:45 +0300

    returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define object attribute lookup function."""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return (dir(obj))
+~                       
