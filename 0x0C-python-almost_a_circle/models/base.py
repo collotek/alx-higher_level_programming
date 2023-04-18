36b055928a3b2e0ae4f67d84ef6aaccb886f4b17
collotek <collinskipkorir94@gmail.com>
2023-04-18 18:01:09 +0300

    the first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defines class: Base"""
+
+Class Base:
+    """ Base class
+    This class will be the “base” of all other classes in this project.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+         """Initializing a new Base.
+        Args:
+            id (int): New Base Identity.
+        """
+        if (id != None):
+            seld.id  = id
+        else:
+            Base.__nb_objects++
+            self.id = Base.__nb_objects
