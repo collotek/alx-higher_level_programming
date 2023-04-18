d458cc0246d223682716a39859f14b8eec5bc263
collotek <collinskipkorir94@gmail.com>
2023-04-18 18:44:37 +0300

    adding corrections

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 
 """Defines class: Base"""
 
+
 Class Base:
     """ Base class
     This class will be the “base” of all other classes in this project.
@@ -10,13 +11,16 @@ Class Base:
         __nb_object (int): Number of instantiated Bases.
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
-         """Initializing a new Base.
+
+        """Initializing a new Base.
         Args:
             id (int): New Base Identity.
         """
-        if (id != None):
-            seld.id  = id
+
+        if (id is not None):
+            self.id = id
         else:
             Base.__nb_objects++
             self.id = Base.__nb_objects
